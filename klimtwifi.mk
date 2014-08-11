586a435a7157027bb5490a607a7c26fcb7802315
Patrick Buckley <ptrck@blck.io>
2014-08-10 21:33:28 -0700

    Sync working tree
    Change-Id: I275cb4444ae825790864381915f1f23821308512

--- klimtwifi.mk
+++ klimtwifi.mk
@@ -143,7 +143,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # System properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=320 \
-    ro.opengles.version=196608 
+    ro.opengles.version=196608 \
+    ro.sapa.jack.master=alsa \
+    ro.hdcp2.rx=tz \
+    ro.secwvk=220 \
+    ro.securestorage.support=true
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -178,13 +182,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=68 \
-    ro.hwui.layer_cache_size=48 \
-    ro.hwui.path_cache_size=10 \
-    ro.hwui.shape_cache_size=2 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=6 \
-    ro.hwui.texture_cache_flush_rate=0.5 \
+    ro.hwui.texture_cache_size=96 \
+    ro.hwui.layer_cache_size=64 \
+    ro.hwui.path_cache_size=16 \
+    ro.hwui.shape_cache_size=4 \
+    ro.hwui.gradient_cache_size=2 \
+    ro.hwui.drop_shadow_cache_size=8 \
+    ro.hwui.texture_cache_flushrate=0.5 \
     ro.hwui.text_small_cache_width=1024 \
     ro.hwui.text_small_cache_height=512 \
     ro.hwui.text_large_cache_width=2048 \
